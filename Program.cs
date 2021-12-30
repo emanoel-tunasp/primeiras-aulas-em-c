53591e8749e9579171a81ecdf8ba7e3f34dc9d1f
emanoel sousa <emanoeltuna3@gmail.com>
2021-12-29 23:37:38 -0300

    primeiroprojeto em c

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aula1console
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string Condicao;
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("como esta você?");
+            Condicao = Console.ReadLine();
+
+            Console.WriteLine(" A sua condição e :" + Condicao);
+
+            Console.ReadKey();
+
+        }
+    }
+}
